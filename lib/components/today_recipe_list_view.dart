3d6e820efbedb23c40d393efda0ee3c14cb410f2
BD911-Coder <fc.developer@icloud.com>
2022-08-17 23:39:51 +0300

    today_recipe_list_view.dart updated

---
+++ lib/components/today_recipe_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodayRecipeListView extends StatefulWidget {
+  TodayRecipeListView({Key? key}) : super(key: key);
+
+  @override
+  State<TodayRecipeListView> createState() => _TodayRecipeListViewState();
+}
+
+class _TodayRecipeListViewState extends State<TodayRecipeListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
